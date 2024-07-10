f8c01c69096ce387f1518f6e38c7a9f95b19e672
LiamLi <zl127@illinois.edu>
2024-07-10 02:09:47 -0500

    POST

--- Express/02-express-app-2.js
+++ Express/02-express-app-2.js
@@ -5,8 +5,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// https://expressjs.com/en/starter/static-files.html
-// express.static is a built-in middleware!
+// It serves static files from a directory, e.g. 'browser-app.js' is loaded in 'localhost:5001/browser-app.js'!
+// It loads the front end project: https://expressjs.com/en/starter/static-files.html
 app.use(express.static('./resources/public'));
 
 app.get('/', (req, res) => {
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, './resources/navbar-app/index.html'));
 });
 
-// Note that since index.html is also a static file, we can actually put index.html to public folder and comment the above piece of code!
+// Note that since index.html is also a static file, we can actually put index.html to public folder and comment the above piece of code! This works because if we navigate to root url ('/'), Express will look for a default file (index.html) to load!!! This means that 'localhost:5001' and 'localhost:5001/index.html' can load the same webpage!!!
 
 app.get('/about', (req, res) => {
     res.status(200).send('<h1>About Page</h1>');
