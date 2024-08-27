795526a87edba5ff0630cf13300691fdadb23d25
LiamLi <zl127@illinois.edu>
2024-08-26 19:08:11 -0500

    unauthenticated error

--- 6-FileUpload/errors/index.js
+++ 6-FileUpload/errors/index.js
@@ -1,6 +1,6 @@
 const CustomError = require('./custom-error');
-const UnauthorizedError = require('./unauthorized');
+const UnauthenticatedError = require('./unauthenticated');
 const NotFoundError = require('./not-found');
 const BadRequestError = require('./bad-request');
 
-module.exports = { CustomError, UnauthorizedError, NotFoundError, BadRequestError };
+module.exports = { CustomError, UnauthenticatedError, NotFoundError, BadRequestError };
